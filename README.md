c5cde7bf4a89659622f6848e5aa66f7656b6d725
xuld <xuld@vip.qq.com>
2017-10-10 13:36:48 +0800

    导入项目

---
+++ README.md
@@ -0,0 +1,23 @@
+﻿# digo-sass-lint
+[digo](https://github.com/digojs/digo) 插件：使用 [Sass-Lint](https://github.com/sasstools/sass-lint) 检查 Sass 代码。
+
+## 安装
+```bash
+npm install digo-sass-lint -g
+```
+
+## 用法
+### 检查 Sass 代码
+```js
+digo.src("*.sass", "*.scss").pipe("digo-sass-lint");
+```
+
+## 选项
+```js
+digo.src("*.sass", "*.scss").pipe("digo-sass-lint", {
+    level: "error",                     // 报错等级。可以是 "error" 或 "warning"。
+    configuration: ".sass-lint.yml",    // 规则文件。详见 https://github.com/sasstools/sass-lint#rules-documentation
+});
+```
+
+另参考 https://github.com/sasstools/sass-lint#rules-documentation。
---
+++ index.js
@@ -0,0 +1,25 @@
+﻿var sasslint = require("sass-lint");
+
+module.exports = function SassLint(file, options) {
+    options = Object.assign({}, options);
+    var result = sasslint.lintFileText({
+        'text': file.content,
+        'format': file.ext.replace('.', ''),
+        'filename': file.srcPath
+    }, options, options.configuration || ".sass-lint.yml");
+
+    for (const output of result.messages) {
+        const e = {
+            plugin: SassLint.name,
+            message: output.ruleId + ": " + output.message,
+            fileName: result.filePath,
+            line: output.line,
+            column: output.character
+        };
+        if (options.level === "warning" || output.severity <= 1) {
+            file.warning(e);
+        } else {
+            file.error(e);
+        }
+    }
+};
